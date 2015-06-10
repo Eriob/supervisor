ed4f3938d4348b0e590f21e9a77c4b01ae00f45e
Alan <alan.barthelemy@gmail.com>
2015-06-10 09:53:38 +0200

    CmanageUsers

--- Controller/CmanageUsers.php
+++ Controller/CmanageUsers.php
@@ -4,7 +4,7 @@
 	if (isset($_SESSION['name'])) {
 		include ('./Model/MmanageUsers.php');
 		include ('./Viewer/VmanageUsers.php');
-		$users = manageUsers();
+		manageUsers();
 	}else{
 		include('./Controller/Cindex.php');
 	}
