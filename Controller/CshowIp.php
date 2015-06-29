e8440a1d7c9d2c399adc43f4647cc217ecfb999c
Alan <alan.barthelemy@gmail.com>
2015-06-29 16:09:11 +0200

    showip

--- Controller/CshowIp.php
+++ Controller/CshowIp.php
@@ -2,7 +2,7 @@
 
 if (isset($_GET['index']){
 	if ($_GET['index'] == "showIp") {
-		include("./Viewer/Explored/hostname_10.8.96.1.php'");
+		include("./Viewer/Explored/hostname_10.8.96.1.php");
 		//include("./Viewer/Explored/hostname_'".$_GET['ip']."'.php'");
 	}
 }
