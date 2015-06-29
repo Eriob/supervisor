2507f0396ec891d5641d8ab4f493562276658620
Alan <alan.barthelemy@gmail.com>
2015-06-29 16:12:07 +0200

    cexplore.php

--- Controller/Cexplore.php
+++ Controller/Cexplore.php
@@ -1,6 +1,6 @@
 <?php
 
-$tableau="";
+$string="";
 
 if(file_exists("/var/www/supervisor/Scripts/hostname_'".$_GET['ip']."'.txt'"){
 
@@ -9,14 +9,14 @@ if(file_exists("/var/www/supervisor/Scripts/hostname_'".$_GET['ip']."'.txt'"){
 	$total = count($fichier); // Nombre total des lignes du fichier
 
 	for($i = 0; $i < $total; $i++){
-		$tableau += $fichier[$i];
+		$string += $fichier[$i]."\n";
 	}
 
 	$open = fopen("/var/www/supervisor/Viewer/Explored/hostname_'".$_GET['ip']."'.php", "w");
-	fwrite($open, $tableau); 
+	fwrite($open, $string); 
 	fclose($open);
 
-	return $tableau;
+	return $string;
 }else{
 	sleep(1);
 }
