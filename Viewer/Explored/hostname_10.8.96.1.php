d655b411c4367a824c55c37ca78f75002cae39f1
Alan <alan.barthelemy@gmail.com>
2015-06-29 16:33:10 +0200

    index

--- Viewer/Explored/hostname_10.8.96.1.php
+++ Viewer/Explored/hostname_10.8.96.1.php
@@ -1,31 +1,5 @@
-        <!-- BEGIN CONTENT -->
-        <div class="page-content-wrapper">
-            <div class="page-content">
-                <!-- BEGIN PAGE HEADER-->
-                <h3 class="page-title">
-                Explored</h3>
-                <div class="page-bar">
-                    <ul class="page-breadcrumb">
-                        <li>
-                            <span class="glyphicon glyphicon-home"></span>
-                            <a href="./index.php">Home</a>
-                            <span class="glyphicon glyphicon-chevron-right"></i>
-                        </li>
-                        <li>
-                            <a href="index.php">Explored</a>
-                        </li>
-                    </ul>
-                    <div class="page-toolbar">
-                    </div>
-                </div>
-                <!-- END PAGE HEADER-->
-                <div class="row">
-                    <div class="col-lg-2 col-md-2 col-xs-2"></div>
-                    <div class="col-lg-6 col-md-6 col-xs-6">
-	    				IP : 10.8.96.1
-						MAC : XX:XX:XX:XX:XX
-						Constructeur : DELL
-						Services ouverts :
-						22/ssh
-                    </div>
-                </div>
+	<br>IP : 10.8.96.1</br>
+	<br>MAC : XX:XX:XX:XX:XX</br>
+	<br>Constructeur : DELL</br>
+	<br>Services ouverts :</br>
+	<br>22/ssh</br>
--- index.php
+++ index.php
@@ -4,97 +4,96 @@ session_start();
 
 header('Content-type: text/html; charset=UTF-8');
 
-include_once('./Controller/Cheader.php');
-include_once('./Controller/Cnavbar.php');
+if ($_GET['index'] == "showIp") {
+    include_once('./Controller/CshowIp.php');
+}else{
+    include_once('./Controller/Cheader.php');
+    include_once('./Controller/Cnavbar.php');
 
-    if (isset ($_GET['index'])) {
-        switch ($_GET['index']) {
-            case "login":
-                include('./Controller/Clogin.php');
-                include_once('./Viewer/Vfooter.php');
-                break;
-            case "discoverAll":
-                include('./Controller/CdiscoverAll.php');
-                include_once('./Viewer/Vfooter.php');
-                break;
-            case "discoverOne":
-                include('./Controller/CdiscoverOne.php');
-                include_once('./Viewer/Vfooter.php');
-                break;
-            case "myProfile":
-                include('./Controller/CmyProfile.php');
-                include_once('./Viewer/Vfooter.php');
-                break;
-            case "inbox":
-                include('./Controller/Cinbox.php');
-                include_once('./Viewer/Vfooter.php');
-                break;
-            case "disconnect":
-                include('./Controller/Cdisconnect.php');
-                include_once('./Viewer/Vfooter.php');
-                break;
-            case "manageRights":
-                include('./Controller/CmanageRights.php');
-                break;
-            case "manageUsers":
-                include('./Controller/CmanageUsers.php');
-                break;
-            case "charts":
-                include('./Controller/Ccharts.php');
-                include_once('./Viewer/Vfooter.php');
-                break;
-            case "aboutus":
-                include('./Controller/Caboutus.php');
-                include_once('./Viewer/Vfooter.php');
-                break;
-            case "products":
-                include('./Controller/Cproducts.php');
-                include_once('./Viewer/Vfooter.php');
-                break;
-            case "contactus":
-                include('./Controller/Ccontactus.php');
-                include_once('./Viewer/Vfooter.php');
-                break;
-            case "searchAll":
-                include('./Controller/CdiscoverAll.php');
-                include_once('./Viewer/Vfooter.php');
-                break;
-            case "ToDo":
-                include('./Controller/CToDo.php');
-                include_once('./Viewer/Vfooter.php');
-                break;
-            case "addProfileSupervision":
-                include('./Controller/CaddProfileSupervision.php');
-                include_once('./Viewer/Vfooter.php');
-                break;
-            case "showTraps":
-                include('./Controller/CshowTraps.php');
-                include_once('./Viewer/Vfooter.php');
-                break;
-            case "createUser":
-                include('./Controller/CcreateUser.php');
-                include_once('./Viewer/Vfooter.php');
-                break;
-            case "validNewUser":
-                include('./Controller/CcreateUser.php');
-                break;
-            case "explore":
-                include_once('./Controller/Cexplore.php');
-                include_once('./Viewer/Vfooter.php');
-                break;
-            case "showIp":
-                include_once('./Controller/CshowIp.php');
-                include_once('./Viewer/Vfooter.php');
-                break;
-            default:
-                include_once('./Controller/Cindex.php');
-                include_once('./Viewer/Vfooter.php');
-                break;
-        }   
-    }else{
-        $_GET['index'] = "index";
-        include_once('./Controller/Cindex.php');
-        include_once('./Viewer/Vfooter.php');
-    }
-
+        if (isset ($_GET['index'])) {
+            switch ($_GET['index']) {
+                case "login":
+                    include('./Controller/Clogin.php');
+                    include_once('./Viewer/Vfooter.php');
+                    break;
+                case "discoverAll":
+                    include('./Controller/CdiscoverAll.php');
+                    include_once('./Viewer/Vfooter.php');
+                    break;
+                case "discoverOne":
+                    include('./Controller/CdiscoverOne.php');
+                    include_once('./Viewer/Vfooter.php');
+                    break;
+                case "myProfile":
+                    include('./Controller/CmyProfile.php');
+                    include_once('./Viewer/Vfooter.php');
+                    break;
+                case "inbox":
+                    include('./Controller/Cinbox.php');
+                    include_once('./Viewer/Vfooter.php');
+                    break;
+                case "disconnect":
+                    include('./Controller/Cdisconnect.php');
+                    include_once('./Viewer/Vfooter.php');
+                    break;
+                case "manageRights":
+                    include('./Controller/CmanageRights.php');
+                    break;
+                case "manageUsers":
+                    include('./Controller/CmanageUsers.php');
+                    break;
+                case "charts":
+                    include('./Controller/Ccharts.php');
+                    include_once('./Viewer/Vfooter.php');
+                    break;
+                case "aboutus":
+                    include('./Controller/Caboutus.php');
+                    include_once('./Viewer/Vfooter.php');
+                    break;
+                case "products":
+                    include('./Controller/Cproducts.php');
+                    include_once('./Viewer/Vfooter.php');
+                    break;
+                case "contactus":
+                    include('./Controller/Ccontactus.php');
+                    include_once('./Viewer/Vfooter.php');
+                    break;
+                case "searchAll":
+                    include('./Controller/CdiscoverAll.php');
+                    include_once('./Viewer/Vfooter.php');
+                    break;
+                case "ToDo":
+                    include('./Controller/CToDo.php');
+                    include_once('./Viewer/Vfooter.php');
+                    break;
+                case "addProfileSupervision":
+                    include('./Controller/CaddProfileSupervision.php');
+                    include_once('./Viewer/Vfooter.php');
+                    break;
+                case "showTraps":
+                    include('./Controller/CshowTraps.php');
+                    include_once('./Viewer/Vfooter.php');
+                    break;
+                case "createUser":
+                    include('./Controller/CcreateUser.php');
+                    include_once('./Viewer/Vfooter.php');
+                    break;
+                case "validNewUser":
+                    include('./Controller/CcreateUser.php');
+                    break;
+                case "explore":
+                    include_once('./Controller/Cexplore.php');
+                    include_once('./Viewer/Vfooter.php');
+                    break;
+                default:
+                    include_once('./Controller/Cindex.php');
+                    include_once('./Viewer/Vfooter.php');
+                    break;
+            }   
+        }else{
+            $_GET['index'] = "index";
+            include_once('./Controller/Cindex.php');
+            include_once('./Viewer/Vfooter.php');
+        }
+}
 ?>
