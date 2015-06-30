089f721855c27da19925717b6000260fc331611e
Alan <alan.barthelemy@gmail.com>
2015-06-30 14:05:31 +0200

    explore

--- Controller/Cexplore.php
+++ Controller/Cexplore.php
@@ -3,8 +3,6 @@
 $exec_script = sprintf('/var/www/supervisor/Scripts/host_explore.pl %s', $_GET['ip']);
 exec($exec_script);
 
-fileCheck();
-
 function fileCheck() {
 
 	if(file_exists("/var/www/supervisor/Viewer/Explored/hostname_'".$_GET['ip']."'.php'")){
@@ -14,5 +12,7 @@ function fileCheck() {
 		fileCheck();
 	}
 }
+
+fileCheck();
 
 ?>
