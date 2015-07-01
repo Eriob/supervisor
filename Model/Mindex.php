38466d0490d9ed556543b9441fd572716f59cc45
Tyronid <cartier@intechinfo.fr>
2015-07-01 13:47:49 +0200

    yo

--- Model/Mindex.php
+++ Model/Mindex.php
@@ -3,6 +3,6 @@ function get_information(){
 $sql = "select * from informations";
 $request = mysql_query($sql) or die (mysql_error());
 $information = mysql_fetch_array($request);
-return $informations;
+return $information;
 }
 ?>
