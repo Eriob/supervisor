6012cd2b0cbee594c85d990d487e875b31567929
Eriob <hallay@intechinfo.fr>
2015-07-01 10:59:02 +0200

    About us

--- Viewer/Vaboutus.php
+++ Viewer/Vaboutus.php
@@ -12,7 +12,7 @@
                             <span class="glyphicon glyphicon-chevron-right"></i>
                         </li>
                         <li>
-                            <a href="index.php?index=aboutus">About Us And me</a>
+                            <a href="index.php?index=aboutus">About Us</a>
                         </li>
                     </ul>
                     <div class="page-toolbar">
@@ -22,6 +22,12 @@
                 <div class="row">
                     <div class="col-lg-3 col-md-3 col-xs-3"></div>
                     <div class="col-lg-6 col-md-6 col-xs-6">
+					The project started in March 2015 as a student project. It has been developed by Alan, Julien and Simon, who are currently studying system and network management at IN’TECH INFO (Ivry-sur-Seine, near Paris, France).<br>
+					The project started from the recording of many complaints from students and teachers aswell. In fact, the school’s network tends sometimes to show some weaknesses due to recent growth of the number of students, and the always increasing resources needed to use the internet comfortably.<br>
+					The main goal was simple, we wanted to offer an information page to the school’s community in order to detect and correct quickly problems. We extended our goal by offering a supervising service to anyone who needs.<br>
+					This project is still evolving as it is not finished, and we are thinking about maybe continuing to work on this project the next semester.  You can contact the team on Simon’s mail: hallay@intechinfo.fr<br>
+					We are open to any note, suggestion, request, job offer, or congratulation messages.<br>
+					We hope your supervisor experience will be great !
 
 
 
