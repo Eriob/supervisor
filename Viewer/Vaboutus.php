3a1c274bce02e0597f57998181e107e1a845ddbf
Eriob <hallay@intechinfo.fr>
2015-07-01 11:17:08 +0200

    about

--- Viewer/Vaboutus.php
+++ Viewer/Vaboutus.php
@@ -19,6 +19,12 @@
                     </div>
                 </div>
                 <!-- END PAGE HEADER-->
+				<div class="row">
+				<div class="col-lg-4 col-md-4 col-xs-4"></div>
+				<div class="col-lg-6 col-md-6 col-xs-6">
+				<h2> Le commencement </h2>
+				</div>
+				</div>
                 <div class="row">
                     <div class="col-lg-3 col-md-3 col-xs-3"></div>
                     <div class="col-lg-6 col-md-6 col-xs-6">
