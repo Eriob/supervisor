b6929227d85705d5d79f504630295b7c0ddbeaf2
Eriob <hallay@intechinfo.fr>
2015-07-05 15:52:55 +0200

    test

--- Model/MshowTraps.php
+++ Model/MshowTraps.php
@@ -1,7 +1,8 @@
-<?
+<?php
 function get_traps ($user_id){
 	$sql = "SELECT id, machine_id, date_received, content, action FROM notifications WHERE user_id = '$user_id'";
 	$request = mysql_query($sql) or die (mysql_error());
 	$result = mysql_fetch_array($request);
 	return $result;
 }
+?>
