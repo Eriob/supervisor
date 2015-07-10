a5b36a8f391d7e4c30d82831780567ea679c9824
Eriob <hallay@intechinfo.fr>
2015-07-10 10:26:42 +0200

    superv

--- Controller/CaddProfileSupervision.php
+++ Controller/CaddProfileSupervision.php
@@ -1,13 +1,13 @@
 <?php
 	include('./Model/MconnectBDD.php');
 	include('./Model/MaddProfileSupervision.php');
-	 /* if (isset($_SESSION['name'])) {
+	if (isset($_SESSION['name'])) {
 		include ('./Viewer/VaddProfileSupervision.php');
 		
 	}
 	else{
 		include('./Controller/Cindex.php');
-	} */
+	}
 
 	if (isset($_POST['public_com'])&& ($_POST['public_com']!= '')){
 		$public_com = $_POST['public_com'];
@@ -46,16 +46,16 @@
 	}
 
 	if (isset($public_com, $private_com, $syscontact, $syslocation, $service, $ins_min, $ins_max, $partition, $charge)){
-	/*$snmpd_conf = fopen('snmpd.conf', 'w');
-	 addSupervision($_SESSION['id'], $public_com, $private_com, $syscontact, $syslocation);	
+	$snmpd_conf = fopen('snmpd.conf', 'w');
+	/* addSupervision($_SESSION['id'], $public_com, $private_com, $syscontact, $syslocation);	
 	$superv_id = get_id($_SESSION['id']);
 	$services = get_services($superv_id);
 	$disks = get_disks($superv_id);
 	addService($data, $service, $ins_min, $ins_max);
-	addDisk($data, $partition, $charge); */
+	addDisk($data, $partition, $charge);
 		
 		
-	$snmpd_conf = fopen('snmpd.conf', 'a+');
+	$snmpd_conf = fopen('snmpd.conf', 'a+'); */
 
 	fputs($snmpd_conf, "##############################\n#Fichier de configuration snmp\n#Généré automatiquement par Supervisor\n##############################\nrocommunity $public_com \nrwcommunity $private_com \nsyscontact $syscontact \nsyslocation $syslocation \ninformsink 10.8.96.6 supervisor \nauthtrapenable 2 \ndefaultMonitors yes \nlinkUpDownNotifications yes \ndisk $partition $charge\nproc $service $ins_max $ins_min \nprocfix $service /etc/init.d/$service restart \n");
 	fclose($snmpd_conf);
