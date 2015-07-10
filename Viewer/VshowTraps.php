58d1c94a8a5c29fa9fa7cf84c88a0962a1a7cf26
Eriob <hallay@intechinfo.fr>
2015-07-10 10:03:57 +0200

    TR

--- Viewer/VshowTraps.php
+++ Viewer/VshowTraps.php
@@ -19,12 +19,5 @@
                     </div>
                 </div>
                 <!-- END PAGE HEADER-->
-                <div class="row">
-                    <div class="col-lg-3 col-md-3 col-xs-3"></div>
-                    <div class="col-lg-6 col-md-6 col-xs-6">
 
-
-
-                    </div>
-                <br><br><br><br><br><br><br><br><br>
    
