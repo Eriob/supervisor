edd8fdcfa9b2f9449b47711f0891c571960b57fa
Eriob <hallay@intechinfo.fr>
2015-07-10 10:36:29 +0200

    sup

--- Viewer/VaddProfileSupervision.php
+++ Viewer/VaddProfileSupervision.php
@@ -22,7 +22,7 @@
                 <div class="row">
                     <div class="col-lg-3 col-md-3 col-xs-3"></div>
                     <div class="col-lg-6 col-md-6 col-xs-6">
-                        <form class="form-horizontal" name="register_form" method="POST" action="index.php?index=valid_contact">
+                        <form class="form-horizontal" name="register_form" method="POST" action="index.php?index=addProfileSupervision">
                             
                             <div class="form-group">
                                 <label for="name" class="control-label">Login</label> 
